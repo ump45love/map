9894dc1b924ca6efe8a58fef09758651fc8c1300
ump45love <lovecein4858@gmail.com>
2021-12-25 01:27:22 +0900

    수정

--- src/p/getcommand.java
+++ src/p/getcommand.java
@@ -1,11 +1,21 @@
 package p;
 
+import java.awt.image.BufferedImage;
+import java.util.List;
+
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.map.MapCanvas;
+import org.bukkit.map.MapRenderer;
+import org.bukkit.map.MapView;
+
+import ij.ImagePlus;
 
 public class getcommand implements CommandExecutor {
 	  @Override
@@ -13,6 +23,19 @@ public class getcommand implements CommandExecutor {
 	    {
 		  Player player = (Player)sender;
 		  placeBlock.place(0,0,0,Material.GLOWSTONE,player.getWorld(),player);
+		  ItemStack item;
+		  ItemStack i = new ItemStack(Material.MAP, 1);
+		  MapView view = Bukkit.createMap(world);
+		  List<MapRenderer> a = view.getRenderers();
+		  MapCanvas test;
+		  ImagePlus imgPlus = new ImagePlus("g.jpg");
+		  BufferedImage img;
+		  test.drawImage(0, 0, img);
+		  a.get(0).render(view, null, player);
+		  for(MapRenderer renderer : view.getRenderers())
+		        view.removeRenderer(renderer);
+		  view.addRenderer(myCustomRenderer);
+		  i.setDurability(view.getId());
 			return false;
 	     }
 }
